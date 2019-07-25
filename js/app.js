d9b81e578844228c91aa0b80f618dfecb7be9070
darrinduane <darrinj2002@gmail.com>
2019-07-24 23:05:49 -0400

    final

--- js/app.js
+++ js/app.js
@@ -5,22 +5,22 @@ jQuery(document).ready(function ($) {
           moveRight();
       }, 3000);
     });
-    
+  
       var slideCount = $('#slider ul li').length;
       var slideWidth = $('#slider ul li').width();
       var slideHeight = $('#slider ul li').height();
       var sliderUlWidth = slideCount * slideWidth;
-      
+  
       $('#slider').css({ width: slideWidth, height: slideHeight });
-      
+  
       $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-      
+  
       $('#slider ul li:last-child').prependTo('#slider ul');
   
       function moveLeft() {
           $('#slider ul').animate({
               left: + slideWidth
-          }, 200, function () {
+          }, 1000, function () {
               $('#slider ul li:last-child').prependTo('#slider ul');
               $('#slider ul').css('left', '');
           });
@@ -29,7 +29,7 @@ jQuery(document).ready(function ($) {
       function moveRight() {
           $('#slider ul').animate({
               left: - slideWidth
-          }, 200, function () {
+          }, 1000, function () {
               $('#slider ul li:first-child').appendTo('#slider ul');
               $('#slider ul').css('left', '');
           });
@@ -37,11 +37,12 @@ jQuery(document).ready(function ($) {
   
       $('a.control_prev').click(function () {
           moveLeft();
+          return false;
       });
   
       $('a.control_next').click(function () {
           moveRight();
+          return false;
       });
   
-  });    
-  
+  });
